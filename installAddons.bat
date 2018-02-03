39eebfcb1dbfee8df3de56b83427c5c51de2d184
Dimitri Belopopsky <dimitri@belopopsky.com>
2018-02-03 21:57:21 +0100

    MAJ script et support windows

--- installAddons.bat
+++
@@ -1,9 +0,0 @@
-git clone https://github.com/kylemcdonald/ofxCv.git ../../../addons/ofxCv
-git clone https://github.com/ShadowMitia/ofxliquidFun.git ../../../addons/ofxLiquidFun
-git clone https://github.com/armadillu/ofxGLFWJoystick.git ../../../addons/ofxGLFWJoystick
-git clone https://github.com/kr15h/ofxPiMapper.git ../../../addons/ofxPiMapper
-git clone https://github.com/thomasgeissl/ofxWiiuse.git ../../../addons/ofxWiiuse
-
-cd ../../../addons/ofxPiMapper && git checkout e93240318b
-cd ../ofxCv && git checkout stable
-cd ../../apps/myApps/ProjetMapping
---
+++ scripts/updatePortals_windows.sh
@@ -0,0 +1,1 @@
+Invoke-WebRequest -OutFile ../bin/data/portals.csv   https://docs.google.com/spreadsheets/d/e/2PACX-1vQqXctLu711Q9bXUpL_F5l1uKdA7ABvwzl0spgPtdrolrolUAO48RKTvDMY8beJd4PDass4wHWU2r8v/pub?output=csv
