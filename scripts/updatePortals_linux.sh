f1f470003aded0f0ee0992225ff9df74a189519c
Dimitri Belopopsky <dimitri@belopopsky.com>
2018-02-03 21:33:07 +0100

    Scripts

---
+++ scripts/updatePortals_linux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget https://docs.google.com/spreadsheets/d/e/2PACX-1vQqXctLu711Q9bXUpL_F5l1uKdA7ABvwzl0spgPtdrolrolUAO48RKTvDMY8beJd4PDass4wHWU2r8v/pub?output=csv --output-document=../bin/data/portals.csv
